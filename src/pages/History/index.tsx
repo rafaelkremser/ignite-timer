58865c046d3a9cb4764ca49b38f366226323cb89
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-08-25 12:57:57 -0300

    feat: history screen

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,80 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Projeto</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Projeto</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Projeto</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Projeto</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Projeto</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Projeto</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Projeto</td>
+              <td>20 minutos</td>
+              <td>Ha 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
