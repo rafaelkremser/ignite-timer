e1040074fb3541aa80cb39aef603c0251a46d615
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-08-24 23:32:56 -0300

    initial commit

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export type ButtonVariant = 'primary' | 'secondary' | 'success' | 'danger'
+
+interface ButtonContainerProps {
+  variant: ButtonVariant
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+  border: 0;
+  margin-right: 5px;
+`
